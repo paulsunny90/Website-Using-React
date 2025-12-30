4dc2349a9b190c212c5d3c7b6c731aa89484006b
paulsunny90 <paulsunny87267@gmail.com>
2025-12-30 17:31:51 +0530

    navbar compltted

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,15 @@
 import { useState } from 'react'
 import Navbar from './components/Navbar'
-const App=()=>{
-  const [theme,setTheme]=useState('light')
-  return(
-     
+const App = () => {
+  const [theme, setTheme] = useState(localStorage.getItem('theme')?localStorage.getItem('theme'):'light')
+  return (
+
     <div className='dark:bg-black relative'>
-      <Navbar theme={theme} setTheme={setTheme}/>
-    </div>
+      <Navbar theme={theme} setTheme={setTheme} />
+    </div> 
   )
 
 }
 
-
-
 export default App
+ 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,27 +1,36 @@
-import React from "react";
+import React, { useState } from "react";
 import assets from "../assets/assets.js"
+import ThemToggleBtn from "./ThemeToggleBtn.jsx";
 
 const navbar = ({ theme, setTheme }) => {
+    const [sidebarOpen, setSidebarOpen] = useState(false)
     return (
-        <div className="flex justify-between items-center px-4 sm:px-12 lg-px-24 xl:px-40 py-4 sticky top-0 z-20 backdrop-blur-xl;font-medium bg-white/50 dark:bg-gary-900/70 ">
+        <div className="flex justify-between items-center px-4 sm:px-12 lg-px-24 xl:px-40 py-4 sticky top-0 z-20 backdrop-blur-xl;font-medium bg-white/50 dark:bg-gray-900/70 ">
             <img src={theme === "dark" ? assets.logo_dark : assets.logo} alt="" className="w-32" />
-            
-            < div className="text-gray-700 dark:text-white sm:text-sm max-sm:w-60 max-sm:pl-10 max-sm:fixed top-0 bottom-0 right-0 max-sm:min-h-screen max-sm:h-full
-             max-sm:flex-col max-sm:bg-primary max-sm:text-white max-sm:pt-20 flex sm:items-center gap-5 transition-all">
 
-                <img src={assets.close_icon} alt="" className="w-5 absolute right-4 top-4 sm:hidden"/>
+            <div
+                className={`text-gray-700 dark:text-white sm:text-sm 
+  ${!sidebarOpen ? 'max-sm:w-0 overflow-hidden' : 'max-sm:w-60 max-sm:pl-10'} 
+  max-sm:fixed top-0 bottom-0 right-0 max-sm:min-h-screen max-sm:h-full max-sm:flex-col max-sm:bg-primary max-sm:text-white max-sm:pt-20 flex sm:items-center gap-5 transition-all`}>
 
-               
-                <a href="#" className="sm:hover:border-b">Home</a>
-                <a href="#Services" className="sm:hover:border-b">Services</a>
-                <a href="#sm:hover:border-b" className="sm:hover:border-b">Our Work</a>
-                <a href="#Contact Us" className="sm:hover:border-b" >Contact Us</a>
+                <img src={assets.close_icon} alt="" className="w-5 absolute right-4 top-4 sm:hidden" onClick={() => setSidebarOpen(false)} />
+
+
+                <a href="#" onClick={() => setSidebarOpen(false)} className="sm:hover:border-b">Home</a>
+                <a href="#Services" onClick={() => setSidebarOpen(false)} className="sm:hover:border-b">Services</a>
+                <a href="#Our Work " onClick={() => setSidebarOpen(false)} className="sm:hover:border-b">Our Work</a>
+                <a href="#Contact Us" onClick={() => setSidebarOpen(false)} className="sm:hover:border-b" >Contact Us</a>
             </div>
-            <div>
-                <a href="#Contact Us"className="text-sm max-sm:hidden flex
+            <div className="flex items-center gap-2 sm:gap-4">
+
+                <ThemToggleBtn theme={theme} setTheme={setTheme} />
+
+                <img src={theme === "dark" ? assets.menu_icon_dark : assets.menu_icon} alt="" onClick={() => setSidebarOpen(true)} className="w-8 sm:hidden" />
+
+                <a href="#Contact Us" className="text-sm max-sm:hidden flex
                     items-center gap-2 bg-primary text-white px-6 py-2 rounded-full 
                      cursor-pointer hover:scale-103 transition-all ">
-                    connect <img src={assets.arrow_icon} alt="arrow_icon" className="width={14}"/>
+                    connect <img src={assets.arrow_icon} alt="arrow_icon" className="width={14}" />
                 </a>
             </div>
         </div>
@@ -30,5 +39,5 @@ const navbar = ({ theme, setTheme }) => {
 
     )
 }
-export default navbar; 
+export default navbar;
 
