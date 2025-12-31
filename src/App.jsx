539fdda48b2c38c5f74fc27bcf26967ed3f6292f
paulsunny90 <paulsunny87267@gmail.com>
2025-12-31 13:27:24 +0530

    services compltted

--- src/App.jsx
+++ src/App.jsx
@@ -13,6 +13,7 @@ const App = () => {
       <Hero/>  
       <TrustedBy />
       <Services/>
+    
      
       
 
--- src/components/Services.jsx
+++ src/components/Services.jsx
@@ -1,9 +1,10 @@
  import React from "react";
 import assets from "../assets/assets";
 import Title from "./Title";
+import ServicesCard from "./ServicesCard";
 
  const Services=()=>{
-    const servicesData = [
+ const servicesData = [
   {
     title: 'Advertising',
     description:
@@ -20,15 +21,30 @@ import Title from "./Title";
     title: 'Social Media Management',
     description:
       'Build a strong social presence with strategic posting, audience engagement, and brand storytelling.',
-    icon: assets.social_ic_
+    icon: assets.social_icon,
+  },
+  {
+    title: 'Content Raiting',
+    description:
+      'We help you  create a maraking stargary that drive result.',
+    icon: assets.content_icon,
   }
-]
+];
+
 
     return(
         <div   id="services" className="relative flex flex-col items-center gap-7 px-4 sm:px-12 lg:px=40 pt-30 text-gray-700  dark-text-white"> 
          <img src={assets.bgImage2} alt="" className="absolute -top-110 -left-70 -z-1 dark:hidden" />
 
          <Title title="how can  we help ?" desc="From strategy to execution, we craft digital solutions that help your business grow and succeed." />
+
+         <div className="flex flex-col md:grid grid-cols-2 ">
+
+          {servicesData .map((Services,index)=>(
+            <ServicesCard key={index} service={Services} index={index}/>
+
+          ))}
+         </div>
 
         </div>
     ) 
