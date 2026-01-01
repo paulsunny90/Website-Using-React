e87d2fc8163dddcf3cdbfb8c39bb3c4a4630b287
paulsunny90 <paulsunny87267@gmail.com>
2026-01-01 20:28:40 +0530

    compltted

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,9 @@
 import { useState } from 'react'
 import Navbar from './components/Navbar'
-import Hero from './components/Hero'
+
+import Hero from './components/hero'
 import TrustedBy from './components/TrustedBy'
-import Services from './components/Services'
+import Services from './components/services.jsx'
 import OurWork from './components/OurWork'
 import Teams from './components/Teams'
 import ContactUs from './components/ContactUs'
